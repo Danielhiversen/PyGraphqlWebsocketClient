cfaa6b78bc2a746dfadad4ed52ea218d78d802cb
Daniel Hjelseth Høyer <mail@dahoiv.net>
2021-07-25 09:10:45 +0200

    callback

--- graphql_subscription_manager/__init__.py
+++ graphql_subscription_manager/__init__.py
@@ -298,7 +298,13 @@ class SubscriptionManager:
 
         self._wait_time_before_retry = 15
 
-        await callback(data)
+        try:
+            await callback(data)
+        except TypeError as exp:
+            if "object NoneType can't be used in 'await' expression"  in str(exp):
+                callback(data)
+                return
+            raise exp
 
     def _cancel_retry_timer(self):
         if self._retry_timer is None:
